775dd12f3d60148419e4f75f04d605a6abcdc385
silverflin <toledorusso@outlook.com>
2024-04-01 16:44:02 -0700

    feat: init commit

---
+++ client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    pb "github.com/silverflin/go-rpc/goguide"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials/insecure"
+)
+
+
+func main(){
+    var opts []grpc.DialOption
+    //grpc.WithTransportCredentials(insecure.NewCredentials())
+    opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    conn, err := grpc.Dial("localhost:50051" , opts...)
+    if err != nil {
+        log.Fatalf("fail to dial: %v", err)
+    }
+    defer conn.Close()
+    client := pb.NewProductsClient(conn)
+
+
+    productList, err := client.GetProductsByPrice(context.Background(),&pb.ProductListRequest{ProductName: "Bagels"})
+    if err != nil {
+        fmt.Println("error at getting products")
+    }
+
+
+    log.Println(productList)
+
+
+}
+// 
