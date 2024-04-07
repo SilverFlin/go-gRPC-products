c67f0ae1623fae4a43cd804ed1a059c917abfc05
silverflin <toledorusso@outlook.com>
2024-04-07 13:50:21 -0700

    chore: add fake data in model

--- internal/model/model.go
+++ internal/model/model.go
@@ -39,8 +39,8 @@ func initializeProducts() []*pb.CompareProductList {
 			Product: &pb.Product{
 				Id:       "1",
 				Name:     "Dougnuts",
-				ImageUrl: "", // Set the appropriate URL if available
-				Details:  "", // Add details if needed
+				ImageUrl: "https://images.unsplash.com/photo-1597586255676-6b33b6268e33?q=80&w=2052&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+				Details:  "Delicious donuts freshly baked every morning",
 			},
 			Prices: []*pb.MarketPrice{
 				{MarketName: "walmart", Price: 30},
@@ -51,14 +51,14 @@ func initializeProducts() []*pb.CompareProductList {
 	// TODO: replace with data from the database or any other source
 	additionalProducts := []*pb.CompareProductList{
 		{
-			Product: &pb.Product{Id: "2", Name: "Bagels", ImageUrl: "", Details: ""},
+			Product: &pb.Product{Id: "2", Name: "Bagels", ImageUrl: "https://images.unsplash.com/photo-1664038082440-ee0a677af315?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", Details: "Freshly baked bagels perfect for breakfast"},
 			Prices: []*pb.MarketPrice{
 				{MarketName: "costco", Price: 25},
 				{MarketName: "local grocery", Price: 27},
 			},
 		},
 		{
-			Product: &pb.Product{Id: "3", Name: "Croissants", ImageUrl: "", Details: ""},
+			Product: &pb.Product{Id: "3", Name: "Croissants", ImageUrl: "https://images.unsplash.com/photo-1623334044303-241021148842?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", Details: "Flaky and buttery croissants made with the finest ingredients"},
 			Prices: []*pb.MarketPrice{
 				{MarketName: "bakery", Price: 35},
 				{MarketName: "cafe", Price: 38},
