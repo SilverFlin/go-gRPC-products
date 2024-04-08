2a22d78e7193e8cf5a3904e4fe295c5043b791cb
silverflin <toledorusso@outlook.com>
2024-04-07 19:47:59 -0700

    feat: add db support

---
+++ internal/database/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func Connect() error {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	ctx := context.TODO()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return err
+	}
+	Client = client
+	return nil
+}
+
+func Close() error {
+	if Client != nil {
+		err := Client.Disconnect(context.TODO())
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,9 +1,28 @@
 package model
 
 import (
-	pb "github.com/silverflin/go-rpc/proto"
+	"context"
+	"fmt"
+	"log"
 	"slices"
+
+	"github.com/silverflin/go-rpc/internal/database"
+	pb "github.com/silverflin/go-rpc/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+var products []*pb.CompareProductList
+
+func init() {
+	err := database.Connect()
+	if err != nil {
+		fmt.Println("Error connecting to MongoDB:", err)
+		return
+	}
+	defer database.Close()
+	products = initializeProducts()
+}
 
 func GetProducts() *pb.ProductList {
 	productList := &pb.ProductList{Product: make([]*pb.Product, 0)}
@@ -26,46 +45,93 @@ func GetPricesFromProduct(productName string) []*pb.MarketPrice {
 	return nil
 }
 
-var products = initializeProducts()
-
 func initializeProducts() []*pb.CompareProductList {
-	// Using make to create a slice of CompareProductList structs with initial values
-	products := make([]*pb.CompareProductList, 0)
 
-	// Assigning initial values to the slice
-	products = append(
-		products,
-		&pb.CompareProductList{
+	collection := database.Client.Database("profeco-products").Collection("CompareProductList")
+
+	collection.DeleteMany(context.Background(), bson.M{})
+	products := []*pb.CompareProductList{
+		{
 			Product: &pb.Product{
-				Id:       "1",
 				Name:     "Dougnuts",
-				ImageUrl: "https://images.unsplash.com/photo-1597586255676-6b33b6268e33?q=80&w=2052&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+				ImageUrl: "https://images.unsplash.com/photo-1597586255676-6b33b6268e33?q=80&w=2052&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH",
 				Details:  "Delicious donuts freshly baked every morning",
 			},
 			Prices: []*pb.MarketPrice{
 				{MarketName: "walmart", Price: 30},
 			},
-		})
+		},
 
-	// Fake data
-	// TODO: replace with data from the database or any other source
-	additionalProducts := []*pb.CompareProductList{
 		{
-			Product: &pb.Product{Id: "2", Name: "Bagels", ImageUrl: "https://images.unsplash.com/photo-1664038082440-ee0a677af315?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", Details: "Freshly baked bagels perfect for breakfast"},
+			Product: &pb.Product{
+				Name:     "Bagels",
+				ImageUrl: "https://images.unsplash.com/photo-1664038082440-ee0a677af315?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M",
+			},
 			Prices: []*pb.MarketPrice{
 				{MarketName: "costco", Price: 25},
 				{MarketName: "local grocery", Price: 27},
 			},
 		},
 		{
-			Product: &pb.Product{Id: "3", Name: "Croissants", ImageUrl: "https://images.unsplash.com/photo-1623334044303-241021148842?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", Details: "Flaky and buttery croissants made with the finest ingredients"},
+			Product: &pb.Product{
+				Name:     "Croissants",
+				ImageUrl: "https://images.unsplash.com/photo-1623334044303-241021148842?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=",
+			},
 			Prices: []*pb.MarketPrice{
 				{MarketName: "bakery", Price: 35},
 				{MarketName: "cafe", Price: 38},
 			},
 		},
-		// Add more products with their prices similarly
 	}
-	products = append(products, additionalProducts...)
+
+	for _, product := range products {
+		result, err := collection.InsertOne(context.Background(), product)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		insertedID, ok := result.InsertedID.(primitive.ObjectID)
+		if !ok {
+			log.Fatal("Failed to convert inserted ID to ObjectID")
+		}
+
+		fmt.Println(insertedID.Hex())
+
+		product.Product.Id = insertedID.Hex()
+	}
+
+	products = products[:0]
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var product pb.CompareProductList
+
+		if err := cursor.Decode(&product); err != nil {
+			log.Fatal(err)
+		}
+
+		var result map[string]interface{}
+
+		err := cursor.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		id, ok := result["_id"].(primitive.ObjectID)
+		if !ok {
+			log.Fatal("Failed to convert _id to ObjectID")
+		}
+
+		product.Product.Id = id.Hex()
+		products = append(products, &product)
+	}
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 	return products
 }
